14cef8e01810adb4ea48bb8216fdcc9f22baadbc
David Janssens <fiammybe@gmail.com>
2021-11-04 21:55:33 +0100

    New translations admin.php (Dutch)

--- language/Dutch/admin.php
+++ language/Dutch/admin.php
@@ -13,20 +13,20 @@
 defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
 
 // Requirements
-define("_AM_CONTENT_REQUIREMENTS", "content Requirements");
-define("_AM_CONTENT_REQUIREMENTS_INFO", "We've reviewed your system, unfortunately it doesn't meet all the requirements needed for content to function. Below are the requirements needed.");
-define("_AM_CONTENT_REQUIREMENTS_ICMS_BUILD", "content requires at least ImpressCMS 1.1.1 RC 1.");
-define("_AM_CONTENT_REQUIREMENTS_SUPPORT", "Should you have any question or concerns, please visit our forums at <a href='http://community.impresscms.org'>http://community.impresscms.org</a>.");
+define("_AM_CONTENT_REQUIREMENTS", "content module vereisten");
+define("_AM_CONTENT_REQUIREMENTS_INFO", "Nazicht van uw systeem geeft aan dat het niet aan alle vereisten voldoet om de content module te kunnen laten functioneren. Hieronder staan de vereisten die nodig zijn.");
+define("_AM_CONTENT_REQUIREMENTS_ICMS_BUILD", "content vereist ten minste ImpressCMS 1.1.1 RC 1.");
+define("_AM_CONTENT_REQUIREMENTS_SUPPORT", "Mocht je vragen of opmerkingen hebben, bezoek dan onze forums op <a href='https://www.impresscms.org/modules/iforum'>https://www.impresscms.org/modules/iforum/</a>.");
 
 // general
-define("_AM_CONTENT_CONTENTS", "Contents");
+define("_AM_CONTENT_CONTENTS", "Inhoud");
 
 // Content
-define("_AM_CONTENT_CONTENT_CREATE", "Add a content");
-define("_AM_CONTENT_CONTENT", "Content");
-define("_AM_CONTENT_CONTENT_EDIT", "Edit this content");
-define("_AM_CONTENT_CONTENT_MODIFIED", "The content was successfully modified.");
-define("_AM_CONTENT_CONTENT_CREATED", "The content has been successfully created.");
-define("_AM_CONTENT_CONTENT_CLONE", "Clone this content");
-define("_AM_CONTENT_PREVIEW", "Preview Content");
-define("_AM_CONTENT_VIEW", "View Full Content Info");
+define("_AM_CONTENT_CONTENT_CREATE", "Inhoud toevoegen");
+define("_AM_CONTENT_CONTENT", "Inhoud");
+define("_AM_CONTENT_CONTENT_EDIT", "Deze inhoud bewerken");
+define("_AM_CONTENT_CONTENT_MODIFIED", "De inhoud is succesvol gewijzigd.");
+define("_AM_CONTENT_CONTENT_CREATED", "De inhoud is succesvol aangemaakt.");
+define("_AM_CONTENT_CONTENT_CLONE", "Deze inhoud dupliceren");
+define("_AM_CONTENT_PREVIEW", "Voorbeeld tonen");
+define("_AM_CONTENT_VIEW", "Bekijk volledige inhoudsinformatie");
